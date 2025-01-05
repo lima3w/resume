4f2a07b469ff3741ecd4214866ba6e78ba865e91
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2025-01-04 21:50:30 -0600

    adding IDs to each individual section

--- js/data.js
+++ js/data.js
@@ -34,6 +34,7 @@ function renderResume(data) {
   const info = data.info;
 
   // Build Header section
+  headerSection.setAttribute("id", "header");
   headerSection.innerHTML = `
     <h1>
         ${info.name}
@@ -44,6 +45,7 @@ function renderResume(data) {
     `;
 
   // Build Info section
+  infoSection.setAttribute("id", "info");
   infoSection.innerHTML = `
         <p class="siteLink">
         ${info.sites
@@ -57,14 +59,17 @@ function renderResume(data) {
     `;
 
   // Build Summary section
+  summarySection.setAttribute("id", "summary");
   summarySection.innerHTML = `<h2>Summary</h2><p>${data.summary}</p>`;
 
   // Build Skills section
+  skillsSection.setAttribute("id", "skills");
   skillsSection.innerHTML = `<h2 class="sub">Skills</h2><ul>${data.skills
     .map((skill) => `<li>${skill}</li>`)
     .join("")}</ul>`;
 
   // Build Work History section
+  workHistorySection.setAttribute("id", "workHistory");
   workHistorySection.innerHTML = `<h2 class="sub">Work History</h2>`;
   data.workHistory.forEach((job) => {
     workHistorySection.innerHTML += `
@@ -79,6 +84,7 @@ function renderResume(data) {
   });
 
   // Build Education section
+  educationSection.setAttribute("id", "education");
   educationSection.innerHTML = `<h2 class="sub">Education</h2>`;
   data.education.forEach((edu) => {
     educationSection.innerHTML += `
@@ -92,21 +98,25 @@ function renderResume(data) {
   });
 
   // Build Certifications section
+  certificationsSection.setAttribute("id", "certifications");
   certificationsSection.innerHTML = `<h2 class="sub">Certifications</h2><ul>${data.certifications
     .map((cert) => `<li>${cert.name} (${cert.date})</li>`)
     .join("")}</ul>`;
 
   // Build Accolades section
+  accoladesSection.setAttribute("id", "accolades");
   accoladesSection.innerHTML = `<h2 class="sub">Accolades</h2><ul>${data.accolades
     .map((accolade) => `<li>${accolade.name} (${accolade.date})</li>`)
     .join("")}</ul>`;
 
   // Build Homelab section
+  homelabSection.setAttribute("id", "homelab");
   homelabSection.innerHTML = `<h2 class="sub">Homelab</h2><ul>${data.homelab
     .map((item) => `<li>${item}</li>`)
     .join("")}</ul>`;
 
   // Build Personal section
+  personalSection.setAttribute("id", "personal");
   personalSection.innerHTML = `<h2 class="sub">Personal</h2>`;
   data.personal.forEach((item) => {
     personalSection.innerHTML += `
